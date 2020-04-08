35c6a124c64142874278a08fade8df3e49cb7de8
kalaimurthy <kalaimurthyj@gmail.com>
2020-04-08 21:52:07 +0530

    First commit

---
+++ src/test/java/com/hcl/pp/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.pp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
